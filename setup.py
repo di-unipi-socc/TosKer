6d9b5815e6bc1bea191f642dd98ac3b53b2907db
lucarin91 <lucarin91@gmail.com>
2016-11-18 14:41:14 +0100

    update version and add slides

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
+    version='0.0.3',
 
     description='A TOSCA engine working with Docker container',
     long_description=long_description,
@@ -85,7 +85,10 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
+    data_files=[
+        ('lib/tosker', ['tosker/tosker-types.yaml',
+                        'tosker/tests/TOSCA/*'])
+    ],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
