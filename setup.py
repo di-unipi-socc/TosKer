cdbacf4264267166fc0b891e38cb34e82893ee63
lucarin91 <lucarin91@gmail.com>
2016-11-20 18:17:05 +0100

    add six dependency

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['docker-py', 'tosca-parser', 'termcolor'],
+    install_requires=['docker-py', 'tosca-parser', 'termcolor', 'six'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
