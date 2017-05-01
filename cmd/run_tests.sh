655956c03aabcdd33b309f46247c4eb01885370e
lucarin91 <lucarin91@gmail.com>
2017-05-01 12:49:03 +0200

    update run_test script

--- cmd/run_tests.sh
+++ cmd/run_tests.sh
@@ -12,6 +12,7 @@ fi
 
 # test on python2
 if [ $# == 0 ] || [ $1 == "python2" ]; then
+  echo 'run python2 tests...'
   echo 'TEST PYTHON2' >> $TEST_LOG
   . ./venv2/bin/activate
   pip install -r requirements.txt &> /dev/null
@@ -22,11 +23,12 @@ fi
 
 # test on python3 and coverage
 if [ $# == 0 ] || [ $1 == "python3" ]; then
+  echo 'run python3 tests...'
   echo "\nTEST PYTHON3 and COVERAGE" >> $TEST_LOG
   . ./venv3/bin/activate
   pip install -r requirements.txt &> /dev/null
-  coverage run --source tosker -m unittest discover 2> /dev/null
-  coverage report -m  --omit 'tosker/tests/*,tosker/graph/*,tosker/helper.py,*__init__.py' >> $TEST_LOG
+  coverage run --source tosker -m unittest discover -v 2> /dev/null
+  coverage report -m --omit 'tosker/tests/*,tosker/graph/*,tosker/helper.py,*__init__.py' >> $TEST_LOG
   coverage html --omit 'tosker/tests/*,tosker/graph/*,tosker/helper.py,*__init__.py'
   deactivate
 fi
