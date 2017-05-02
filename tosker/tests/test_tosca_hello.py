3b0f4f71fe9042ea11d885279fbcd147021e7a34
lucarin91 <lucarin91@gmail.com>
2017-05-02 21:41:05 +0200

    redirect output

--- tosker/tests/test_tosca_hello.py
+++ tosker/tests/test_tosca_hello.py
@@ -20,17 +20,19 @@ class Test_Hello(Test_Orchestrator):
     def setUp(self):
         super(self.__class__, self).setUp()
         self.orchestrator = Orchestrator(quiet=False)
-        self.orchestrator._parse('tosker/tests/TOSCA/hello.yaml')
+        with redirect_stdout(StringIO()):
+          self.orchestrator._parse('tosker/tests/TOSCA/hello.yaml')
 
     def test(self):
-        self.create()
-        self.start_check_exit()
-        self.stop()
-        self.orchestrator._parse('tosker/tests/TOSCA/hello.yaml')
-        self.start_check_exit()
-        self.stop()
-        con_id = self._docker.inspect_container('hello_container')['Id']
-        self.delete()
+        with redirect_stdout(StringIO()):
+            self.create()
+            self.start_check_exit()
+            self.stop()
+            self.orchestrator._parse('tosker/tests/TOSCA/hello.yaml')
+            self.start_check_exit()
+            self.stop()
+            con_id = self._docker.inspect_container('hello_container')['Id']
+            self.delete()
 
         # verify output
         temp_stdout = StringIO()
